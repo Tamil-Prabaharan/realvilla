0f0e852f96cc6b0ecdba8133ba0f3fbbbb592de1
tamilprabaharan <tamilprabaharancivil@gmail.com>
2025-05-17 02:09:26 +0530

    testing branch2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home'
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>hi welcome to every one</h1>
+      <h1>hi welcome to every one</h1>
+      <h1>hi welcome to every one</h1>
+      <Home />
+      <About />
     </div>
   );
 }
